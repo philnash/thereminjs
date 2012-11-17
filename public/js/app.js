52334199cf44d2c018984a77fae1b39ccb9522cd
Phil Nash <philnash@gmail.com>
2012-11-17 14:51:21 +0000

    objectdetect with coords

--- public/js/app.js
+++ public/js/app.js
@@ -12,8 +12,24 @@ Theremin.Video = function(options, success, error){
 
 window.addEventListener('load', function(){
   Theremin.Video({video:true}, function(stream){
-    var video = document.getElementsByTagName('video')[0];
+    var video = document.getElementsByTagName('video')[0],
+        square = $('#hand'),
+        tick =  function(){
+                  $(video).objectdetect('all', {classifier: objectdetect.handopen}, function(coords){
+                    if(coords[0]){
+                      console.log(coords[0]);
+                      square.css({
+                        left: coords[0][0],
+                        top: coords[0][1]
+                      });
+                    }
+                  });
+                  window.requestAnimationFrame(tick);
+                };
     video.src = window.URL.createObjectURL(stream);
+
+    window.requestAnimationFrame(tick);
+
   }, function(){
     console.log('something went wrong');
   })
